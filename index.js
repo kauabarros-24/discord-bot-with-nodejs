a9f89e2e37d24fe7c11472def0a59d2cdee3cd84
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-06 17:52:27 -0300

    feat: responder com pong

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+
 const dotenv = require('dotenv')
 dotenv.config()
 const {TOKEN, CID, GID} = process.env
+
+//Comandos
+const fs = require('fs')
+const path = require("node:path")
+const commandsPath = path.join(__dirname, "commands")
+var commandsFile = fs.readSync(commandsPath).filter(file => f)
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
