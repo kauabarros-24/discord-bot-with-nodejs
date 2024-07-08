2b018114c035363029708bf01321166fb798c570
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-08 18:02:32 -0300

    Deploy do commands

---
+++ deploy-commands.js
@@ -0,0 +1,3 @@
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
-const dotenv = require('dotenv')
-dotenv.config()
-const {TOKEN, CID, GID} = process.env
-
-//Comandos
-const fs = require('fs')
-const path = require("node:path")
-const commandsPath = path.join(__dirname, "commands")
-var commandsFile = fs.readSync(commandsPath).filter(file => f)
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.login(TOKEN);
+
+// Comandos
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`Esse comando ${filePath} está com dados ou executes errados`);
+    }
+}
+
+// Handling command interactions
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Houve um erro ao tentar executar esse comando!', ephemeral: true });
+    }
+});
+
+//Listener de interações com o bot
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return
+	console.log(interaction)
+
+})
