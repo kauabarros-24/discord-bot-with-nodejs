73740fb91c28e154562b3e82f1b30838dc45e9fb
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-09 16:25:44 -0300

    Primeiro comando funcionando

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 
+// dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+const { TOKEN } = process.env
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// importação dos comandos
+const fs = require("node:fs")
+const path = require("node:path")
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.commands = new Collection()
 
-client.login(TOKEN);
-
-// Comandos
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`Esse comando ${filePath} está com dados ou executes errados`);
-    }
+for (const file of commandFiles){
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command)
+    } else  {
+        console.log(`Esse comando em ${filePath} está com "data" ou "execute ausentes"`)
+    } 
 }
 
-// Handling command interactions
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'Houve um erro ao tentar executar esse comando!', ephemeral: true });
-    }
+// Login do bot
+client.once(Events.ClientReady, c => {
+	console.log(`Pronto! Login realizado como ${c.user.tag}`)
 });
+client.login(TOKEN)
 
-//Listener de interações com o bot
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return
-	console.log(interaction)
-
+// Listener de interações com o bot
+client.on(Events.InteractionCreate, async interaction =>{
+    if (!interaction.isChatInputCommand()) return
+    const command = interaction.client.commands.get(interaction.commandName)
+    if (!command) {
+        console.error("Comando não encontrado")
+        return
+    }
+    try {
+        await command.execute(interaction)
+    } 
+    catch (error) {
+        console.error(error)
+        await interaction.reply("Houve um erro ao executar esse comando!")
+    }
 })
