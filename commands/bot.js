751a6a00dcf7e55b6da8211ba111dba693cd7d47
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-13 14:25:54 -0300

    Comandos para falar do bot

--- commands/bot.js
+++ commands/bot.js
@@ -1,28 +1,21 @@
-const {SlashCommandBuilder, EmbedBuilder, Embed} = require('discord.js')
-const { execute } = require('./ping')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const exampleEmbed = new EmbedBuilder()
     .setColor('Green')
     .setTitle("Sobre o bot: ")
     .addFields(
-        {name: "Dono", value: "Kauã Martins Barros", inline: true},
-        {name: "email", value:"martinsbarroskaua85@gmail", inline: true},
-        {name: "github", value: ""}
+        { name: "Dono", value: "Kauã Martins Barros", inline: true },
+        { name: "Email", value: "martinsbarroskaua85@gmail.com", inline: true },
+        { name: "Profile", value: "https://github.com/kauabarros-24", inline: true }
     )
-    .addFields (
-        {name: "Profile", value: "https://github.com/kauabarros-24?tab=repositories", inline: true}
-    )
-    
-    .setFooter(
-        {text: 'Todos os direitos reservados'}
-    )
+    .setFooter({ text: 'Todos os direitos reservados' });
 
 module.exports = {
-    data: new  SlashCommandBuilder()
-    .setName("bot")
-    .setDescription("Sobre o bot"),
+    data: new SlashCommandBuilder()
+        .setName("bot")
+        .setDescription("Sobre o bot"),
     
     async execute(interaction) {
-        await interaction.reply({embeds: [exampleEmbed]})
+        await interaction.reply({ embeds: [exampleEmbed] });
     }
-}
+};
